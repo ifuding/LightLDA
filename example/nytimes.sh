9978d61470e2b92b68c08f50146cd06a6a6960be
ifuding <fu_ding1991@qq.com>
2017-12-12 05:05:32 -0800

    run on windows

--- example/nytimes.sh
+++ example/nytimes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -x
 root=`pwd`
 echo $root
 bin=$root/bin
@@ -17,7 +17,7 @@ cd $dir
 #python $root/example/text2libsvm.py $dir/docword.nytimes.txt $dir/vocab.nytimes.txt $dir/nytimes.libsvm $dir/nytimes.word_id.dict
 
 # 3. libsvm format to binary format
-$bin/dump_binary $dir/nytimes.libsvm $dir/nytimes.word_id.dict $dir 0
+$bin/dump_binary.exe "$dir/nytimes.libsvm" $dir/nytimes.word_id.dict $dir 0
 
 # 4. Run LightLDA
-$bin/lightlda -num_vocabs 111400 -num_topics 1000 -num_iterations 1 -alpha 0.1 -beta 0.01 -mh_steps 2 -num_local_workers 8 -num_blocks 1 -max_num_document 300000 -input_dir $dir -data_capacity 800
+$bin/lightlda.exe -num_vocabs 111400 -num_topics 1000 -num_iterations 1 -alpha 0.1 -beta 0.01 -mh_steps 2 -num_local_workers 8 -num_blocks 1 -max_num_document 300000 -input_dir $dir -data_capacity 800
